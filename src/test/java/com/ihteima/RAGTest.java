a30197a881909ff2c0b6bd74cb649d0acdfebe84
DeepFuture <12919034+gongkaiming@user.noreply.gitee.com>
2025-10-28 22:13:36 +0800

    Document and Token Test

--- src/test/java/com/ihteima/RAGTest.java
+++ src/test/java/com/ihteima/RAGTest.java
@@ -4,6 +4,12 @@ import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.data.document.parser.TextDocumentParser;
 import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentByParagraphSplitter;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.onnx.HuggingFaceTokenizer;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -51,5 +57,42 @@ public class RAGTest {
                 new ApachePdfBoxDocumentParser()
         );
         System.out.println(document);
+    }
+
+    @Test
+    public void testDocumentSplitter() {
+        //使用FileSystemDocumentLoader读取指定目录下的知识库文档
+        //并使用默认的文档解析器对文档进行解析(TextDocumentParser)
+        Document document = FileSystemDocumentLoader.loadDocument("E:/knowledge/人工智能.md");
+        //为了简单起见，我们暂时使用基于内存的向量存储
+        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        //自定义文档分割器
+        //按段落分割文档：每个片段包含不超过 300个token，并且有 30个token的重叠部分保证连贯性
+        //注意：当段落长度总和小于设定的最大长度时，就不会有重叠的必要。
+        DocumentByParagraphSplitter documentSplitter = new DocumentByParagraphSplitter(
+                300,
+                30,
+                //token分词器：按token计算
+                new HuggingFaceTokenizer());
+        //按字符计算
+        //DocumentByParagraphSplitter documentSplitter = new DocumentByParagraphSplitter(300, 30);
+        EmbeddingStoreIngestor
+                .builder()
+                .embeddingStore(embeddingStore)
+                .documentSplitter(documentSplitter)
+                .build()
+                .ingest(document);
+
+    }
+
+    @Test
+    public void testTokenCount() {
+        String text = "这是一个示例文本，用于测试 token 长度的计算。";
+        UserMessage userMessage = UserMessage.userMessage(text);
+        //计算 token 长度
+//QwenTokenizer tokenizer = new QwenTokenizer(System.getenv("DASH_SCOPE_API_KEY"),"qwen-max");
+        HuggingFaceTokenizer tokenizer = new HuggingFaceTokenizer();
+        int count = tokenizer.estimateTokenCountInMessage(userMessage);
+        System.out.println("token长度：" + count);
     }
 }
